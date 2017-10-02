5d55a6915f5857a84a56aaa82169b058de881fdb
Philipp Claßen <philipp@cliqz.com>
2017-10-02 11:14:08 +0200

    Updated image

--- deploy/terraform/live/dev/services/groupsign/ami.tf
+++ deploy/terraform/live/dev/services/groupsign/ami.tf
@@ -1,5 +1,5 @@
 # "ami.tf" was automatically generated from the template "ami.tf.template".
 variable "ami" {
-  default     = "ami-8e42f1e1"
+  default     = "ami-1b932174"
   description = "The latest AMI of the group sign service."
 }
