372feb809d4332d2aeeef4dbe73bf9c6ea7419d1
Philipp Claßen <philipp@cliqz.com>
2018-01-08 14:58:57 +0100

    Upgrade to Ubuntu 17.10

--- deploy/terraform/live/dev/services/groupsign/ami.tf
+++ deploy/terraform/live/dev/services/groupsign/ami.tf
@@ -1,5 +1,5 @@
 # "ami.tf" was automatically generated from the template "ami.tf.template".
 variable "ami" {
-  default     = "ami-decb74b1"
+  default     = "ami-67d64408"
   description = "The latest AMI of the group sign service."
 }
