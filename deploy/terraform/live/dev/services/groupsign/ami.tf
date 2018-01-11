930c69a816905f8575d73e5092e34e782b535064
Philipp Claßen <philipp@cliqz.com>
2018-01-11 18:54:34 +0100

    Deployed changes

--- deploy/terraform/live/dev/services/groupsign/ami.tf
+++ deploy/terraform/live/dev/services/groupsign/ami.tf
@@ -1,5 +1,5 @@
 # "ami.tf" was automatically generated from the template "ami.tf.template".
 variable "ami" {
-  default     = "ami-67d64408"
+  default     = "ami-1eec7171"
   description = "The latest AMI of the group sign service."
 }
--- deploy/terraform/live/prod/services/groupsign/ami.tf
+++ deploy/terraform/live/prod/services/groupsign/ami.tf
@@ -1,5 +1,5 @@
 # "ami.tf" was automatically generated from the template "ami.tf.template".
 variable "ami" {
-  default     = "ami-a5c37cca"
+  default     = "ami-89ec71e6"
   description = "The latest AMI of the group sign service."
 }
