015b016c416c63a7c188addc4b239502f09f04f0
Alex Catarineu <alex@cliqz.com>
2019-02-25 17:04:18 +0100

    Explain how to change the curve and support for BLS383

--- build-common.sh
+++ build-common.sh
@@ -11,7 +11,10 @@ set -x
     echo "2eab9a13e9558d1fcaf5acf166ba62fd1d945df706743103cfd694c400e2a688 config64.py" | sha256sum -c - && \
     sed -i "s/os.system(\"gcc/os.system(\"$CC $CFLAGS /g" config64.py && \
     sed -i "s/os.system(\"ar/os.system(\"$AR/g" config64.py && \
-    echo -e "18\n0" | python3 config64.py)
+    # Note: this should be in sync with the CURVE choices that we want to support.
+    # Right now, only selecting BN254 = 18, but selecting more than one curve is possible.
+    # Each choice needs to be separated by endline, and last one should be 0.
+    echo -e "18\n20\n0" | python3 config64.py)
 
 $CC $CFLAGS -D AMCL_CURVE_${CURVE} -c core/group-sign.c \
 -I$BUILDFOLDER \
