ca07631bd3576690a269ff9601871fde4c431b6c
Philipp Claßen <philipp@cliqz.com>
2020-01-14 12:31:50 +0100

    Upgrade to latest Milagro version

--- build-common.sh
+++ build-common.sh
@@ -15,14 +15,14 @@ set -x
     cd $BUILDFOLDER && \
     cp $SCRIPTPATH/external/amcl/c/* . && \
     # WARNING: if this config64.py changes, double check the replacements still work
-    echo "29042a5486803a375e3fcbe8334771676e745075666e22ee34e60c784a39dad2 config64.py" | sha256sum -c - && \
+    echo "495a972a8833b6e3313ca50aafb2bfb70dfeed1f83c0d633042e3eb21df0ff32 config64.py" | sha256sum -c - && \
     sed -i "s/os.system(\"gcc/os.system(\"$CC $CFLAGS /g" config64.py && \
     sed -i "s/os.system(\"ar/os.system(\"$AR/g" config64.py && \
     # Note: this should be in sync with the CURVE choices that we want to support.
-    # Select BN254 = 21 and 23 = BLS12383.
+    # Select BN254 = 25 and 27 = BLS12383.
     # (These curves can be used by changing the CURVE file; more details can be found in the README).
     # Each choice needs to be separated by endline, and last one should be 0.
-    echo -e "21\n23\n0" | python3 config64.py)
+    echo -e "25\n27\n0" | python3 config64.py)
 
 $CC $CFLAGS -D AMCL_CURVE_${CURVE} -c core/group-sign.c \
 -I$BUILDFOLDER \
